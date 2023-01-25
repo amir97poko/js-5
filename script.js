4201890fd4b83413fe6c14d8d42a97caae13ea07
amir <amirgafarov09@gmail.com>
2023-01-25 21:53:04 +0500

    full

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+do{
+    var a = +prompt('ведите число');
+    var c = 1;
+}while (isNaN(c && a) || c && a == null || c && a == ''){
+
+    for (let i = 0; i < a; i++) {
+        c = c * a;
+        console.log(c);
+        alert('ваша написаная степень: ' + c);
+    }
+}
+
+
+
+
